e3851ad34b6a2bfcb6507f2ada0bb9513475f847
olamide1212 <154318596+olamide1212@users.noreply.github.com>
2024-02-08 22:19:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//document.addEventListener("scroll", function(){
+//    var main = document.querySelector("main");
+//    main.classList.toggle("sticky", document.scolly > 0);
+//})
+
+console.log('menu')
+let menu = document.getElementsByClassName('menu')
+console.log(menu)
+menu.addEventListener('click', magic)
+// beep.addEventListener('dblclick', bagik)
+// function bagik(){
+//     console.log('double click happend!!!')
+// }
